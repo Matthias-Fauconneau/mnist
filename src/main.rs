1914c1033070a44b6fd192c4be9f6bb902725876
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2023-11-22 17:34:35 +0100

    Stochastic gradient descent for the logistic regression model

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-#![feature(array_chunks, generic_arg_infer, array_methods)]
+#![feature(array_chunks, generic_arg_infer, array_methods)]#![allow(non_snake_case)]
 fn main() -> std::io::Result<()> {
-	let mut weights = [[0.; 28*28]; 10];
-
 	let images = |path| -> std::io::Result<_> {
 		let images = std::fs::read(path)?;
 		let images = &images[4*std::mem::size_of::<u32>()..];
@@ -26,13 +24,10 @@ fn main() -> std::io::Result<()> {
 		Ok((images, labels))
 	};
 
-	let feedforward = |weights: &[[f64; _]; _], image:&[f64; 28*28]| -> [f64; 10] {
-		std::array::from_fn(|i| weights[i].iter().zip(image).map(|(w,i)| w*i).sum())
-	};
-	
 	let (train_images, train_labels) = set("train")?;
 	let train = || train_images.iter().zip(&train_labels);
-	let start = std::time::Instant::now();
+	
+	// Mean example
 	let mut mean_examples = [[0.; 28*28]; 10];
 	let mut label_example_count = [0; 10];
 	for (image, &label) in train() {
@@ -42,16 +37,29 @@ fn main() -> std::io::Result<()> {
 	for label in 0..10 {
 		for j in 0..28*28 { mean_examples[label][j] /= label_example_count[label] as f64; }
 	}
+
+	// Stochastic gradient descent for the logistic regression model
+	fn rand<const N: usize>() -> [f64; N] {use rand::Rng; rand::thread_rng().gen::<[_; _]>().map(|u:f64| u*2.-1.)}
+	let mut theta : [[f64; 28*28]; 10] = [(); 10].map(|_| rand());
+	let mut b: [f64; 10] = rand();
+
+	let F = |weights: &[[f64; _]; _], b: &[_; _], image:&[f64; 28*28]| -> [f64; 10] {
+		let y = std::array::from_fn(|i| weights[i].iter().zip(image).map(|(w,i)| w*i).sum::<f64>() + b[i]);
+		let y = y.map(|y| f64::exp(y));
+		let sum = y.iter().sum::<f64>();
+		y.map(|y| y/sum)
+	};
 	
-	for _ in 0..2 {
-		for (image, &label) in train() {
-			let prediction = feedforward(&weights, image);
-			let target: [_; 10] = std::array::from_fn(|i| if i == label { 1. } else { 0. });
-			for i in 0..10 { 
-				let e_i = target[i]-prediction[i];
+	let start = std::time::Instant::now();
+	for _ in 0..1 {
+		for (x, &y) in train() {
+			let ey: [_; 10] = std::array::from_fn(|k| if k == y { 1. } else { 0. });
+			let F = F(&theta, &b, x);
+			for i in 0..10 {
 				for j in 0..28*28 {
-					weights[i][j] += 2.*e_i*image[j]
+					theta[i][j] -= -(ey[i] - F[i]*x[j]);
 				}
+				b[i] -= -(ey[i] - F[i]);
 			}
 		}
 	}
@@ -64,16 +72,16 @@ fn main() -> std::io::Result<()> {
 			iter.into_iter().enumerate().max_by(|(_, a),(_, b)| f64::total_cmp(a,b)).map(|(i,_)| i).unwrap()
 		}
 		let distance = |a: &[f64; _], b| a.iter().zip(b).map(|(a,b)| sq(a-b)).sum::<f64>();
-		let prediction = match "nearest" { 
-			"nearest" => {
+		let prediction = match "feedforward" { 
+			"nearest" => { // 82%
 				let prediction = mean_examples.each_ref().map(|example| -distance(example, image));
 				max_position(prediction)
 			},
 			"feedforward" => {
-				let prediction = feedforward(&weights, image);
+				let prediction = F(&theta, &b, image);
 				max_position(prediction)
 			},
-			"kNN" => {
+			"kNN" => { // slow
 				let mut distances = Vec::from_iter(train().map(|(example, &label)| (distance(image, example), label)));
 				let (k_nearest, _, _) = distances.select_nth_unstable_by(5, |(a,_),(b,_)| f64::total_cmp(a,b));
 				let counts: [_; 10] = std::array::from_fn(|i| k_nearest.into_iter().filter(|(_,j)| i==*j).count());
@@ -81,7 +89,7 @@ fn main() -> std::io::Result<()> {
 			},
 			_ => unreachable!()
 		};
-		println!("{prediction} {label}");
+		//println!("{prediction} {label}");
 		prediction == *label
 	}).count();
 	println!("{matches}/{} = {}%", labels.len(), matches as f64/labels.len() as f64*100.);
